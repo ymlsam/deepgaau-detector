93fbbc62f413c6f33149ec00f8aa53ef23af19bd
Sam Yuen <ymlsam@gmail.com>
2020-11-24 18:35:16 +0800

    add sample ducky images

--- op/img/split.sh
+++ op/img/split.sh
@@ -12,3 +12,5 @@ rm "$DGD_DATA/test/"*
 # split source dataset into train/dev/test datasets
 echo "splitting into train/dev/test datasets"
 python src/helper/split_dataset.py -i "$DGD_DATA/raw" -o "$DGD_DATA"
+
+# for illustration of eager few-shot learning, you may manually clean up the "train" folder after running above script, and copy "ducky.0" to "ducky.4" (both jpg & xml files) from "raw" folder to "train" folder
